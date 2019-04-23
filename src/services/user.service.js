6294ce841a7d237da832b4db43700b9a17d4c5b6
Lucas Natoli <lucas.natoli@gmail.com>
2019-04-23 16:28:51 -0300

    url correction

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import { authHeader } from '../helpers';
 
 
 const config = {
-    apiUrl : 'http://bigos'
+    apiUrl : 'http://bigos.lan/wp-json/wp-react/v2'
 }
 
 export const userService = {
@@ -22,7 +22,7 @@ function login(username, password) {
         body: JSON.stringify({ username, password })
     };
 
-    return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
+    return fetch(`${config.apiUrl}/login`, requestOptions)
         .then(handleResponse)
         .then(user => {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
