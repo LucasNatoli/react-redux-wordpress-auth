d29ab72d428d365e2d07ef2324f4ee49d3df7e48
Lucas Natoli <lucas.natoli@gmail.com>
2019-04-25 21:29:54 -0300

    homepage doc formatting

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -5,50 +5,50 @@ import { connect } from 'react-redux';
 import { userActions, zubiActions } from '../actions';
 
 class HomePage extends React.Component {
-    componentDidMount() {
-        //this.props.dispatch(userActions.getAll());
-        this.props.dispatch(zubiActions.getMisConsultorias());
-        this.props.dispatch(zubiActions.getMisCapacitaciones());
-    }
+	componentDidMount() {
+		//this.props.dispatch(userActions.getAll());
+		this.props.dispatch(zubiActions.getMisConsultorias());
+		this.props.dispatch(zubiActions.getMisCapacitaciones());
+	}
 
-    handleDeleteUser(id) {
-        return (e) => this.props.dispatch(userActions.delete(id));
-    }
+	handleDeleteUser(id) {
+		return (e) => this.props.dispatch(userActions.delete(id));
+	}
 
-    render() {
-        const { user, users, zubi } = this.props;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h1>Hola {user.firstName}!</h1>
+	render() {
+		const { user, users, zubi } = this.props;
+		return (
+			<div className="col-md-6 col-md-offset-3">
+				<h1>Hola {user.firstName}!</h1>
 
-                <h3>Consultorias</h3>
-                {zubi.loadingConsultorias && <em>Cargando tus consultorias...</em>}
-                {zubi.error && <span className="text-danger">ERROR: {zubi.error}</span>}
-                {zubi.consultorias &&
-                    <ul>
-                        {zubi.consultorias.map((consultoria, index) => 
-                            <li key={consultoria.ID}>
-                                {consultoria.post_title}
-                            </li>
-                        )}
-                    </ul>
-                }
-                
-                <h3>Capacitaciones</h3>
-                {zubi.loadingCapacitaciones && <em>Cargando tus capacitaciones...</em>}
-                {zubi.error && <span className="text-danger">ERROR: {zubi.error}</span>}
-                {zubi.capacitaciones &&
-                    <ul>
-                        
-                        {zubi.capacitaciones.map((capacitacion, index) => 
-                            <li key={capacitacion.ID}>
-                                {capacitacion.post_title}
-                            </li>
-                        )}
-                    </ul>
-                }
+				<h3>Consultorias</h3>
+				{zubi.loadingConsultorias && <em>Cargando tus consultorias...</em>}
+				{zubi.error && <span className="text-danger">ERROR: {zubi.error}</span>}
+				{zubi.consultorias &&
+					<ul>
+						{zubi.consultorias.map((consultoria, index) =>
+							<li key={consultoria.ID}>
+								{consultoria.post_title}
+							</li>
+						)}
+					</ul>
+				}
 
-{/*                 <h3>All registered users:</h3>
+				<h3>Capacitaciones</h3>
+				{zubi.loadingCapacitaciones && <em>Cargando tus capacitaciones...</em>}
+				{zubi.error && <span className="text-danger">ERROR: {zubi.error}</span>}
+				{zubi.capacitaciones &&
+					<ul>
+
+						{zubi.capacitaciones.map((capacitacion, index) =>
+							<li key={capacitacion.ID}>
+								{capacitacion.post_title}
+							</li>
+						)}
+					</ul>
+				}
+
+				{/*                 <h3>All registered users:</h3>
                 {users.loading && <em>Loading users...</em>}
                 {users.error && <span className="text-danger">ERROR: {users.error}</span>}
                 {users.items &&
@@ -65,23 +65,23 @@ class HomePage extends React.Component {
                         )}
                     </ul>
                 } */}
-                <p>
-                    <Link to="/login">Logout</Link>
-                </p>
-            </div>
-        );
-    }
+				<p>
+					<Link to="/login">Logout</Link>
+				</p>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-    const { users, authentication } = state;
-    const { user } = authentication;
-    const { zubi } = state
-    return {
-        user,
-        users,
-        zubi
-    };
+	const { users, authentication } = state;
+	const { user } = authentication;
+	const { zubi } = state
+	return {
+		user,
+		users,
+		zubi
+	};
 }
 
 const connectedHomePage = connect(mapStateToProps)(HomePage);
